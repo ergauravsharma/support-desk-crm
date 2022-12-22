1b0b9e2d11d2e3d30163579fe96e8e2d48b3a45b
Gaurav Sharma <gauravs_iet@yahoo.com>
2022-12-23 00:36:32 +0530

    git commit -m "first commit"

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const colors = require('colors')
+require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+const PORT = process.env.PORT || 5000
+
+// Connect to database
+connectDB()
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+
+app.get('/',(req,res) => {
+    res.send('Hello')
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+app.use(errorHandler)
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
